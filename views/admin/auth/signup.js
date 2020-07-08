b0a38a8729083feae9d9193f708bff8c6a19b171
Vaidotas <skema.vaidotas@gmail.com>
2020-07-08 22:38:37 +0300

    add layout file and proper files structure

---
+++ views/admin/auth/signup.js
@@ -0,0 +1,18 @@
+const layout = require('../layout');
+
+module.exports = ({ req }) => {
+    return layout({ content:
+            `       
+             <div>
+             ${req.session.userId}
+                <form method="POST">
+                    <input name="email" placeholder="email">
+                    <input name="password" placeholder="password">
+                    <input name="passwordConfirmation" placeholder="password confirmation">
+                    <button>Sign Up</button>
+                </form>
+             </div>
+        `
+    });
+
+};
