37e33ebd0262dd4b77743058532689d71a1be05d
Vaidotas <skema.vaidotas@gmail.com>
2020-07-10 18:07:00 +0300

    add sign up validators

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const {check, validationResult} = require('express-validator');
+
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
+const { requireEmail, requirePassword, requirePasswordConfirmation } = require('./validators');
 
 const router = express.Router();
 
@@ -10,20 +13,20 @@ router.get('/signup', (req, res) => {
 });
 
 
-router.post('/signup', async (req, res) => {
+router.post('/signup', [requireEmail, requirePassword, requirePasswordConfirmation], async (req, res) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.send(signupTemplate( {req, errors}));
+    }
+
     const { email, password, passwordConfirmation } = req.body;
 
-    const existingUser = await usersRepo.getOneBy({email: email});
 
-    if(existingUser) {
-        return res.send('Email in use');
-    }
-    if (password !== passwordConfirmation) {
-        return res.send('Passwords must match');
-    }
-
+    // Create a user in our repo to represent this person
     const user = await usersRepo.create({email: email, password: password});
 
+    // Store the id of that user inside the users cookie
     req.session.userId = user.id;
 
 
--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,14 +1,26 @@
 const layout = require('../layout');
 
-module.exports = ({ req }) => {
+const getError = (errors, prop) => {
+    //prop === 'email' || 'password' || 'passwordConfirmation'
+    try{
+        return errors.mapped()[prop].msg
+    } catch (err) {
+        return '';
+    }
+};
+
+module.exports = ({ req, errors }) => {
     return layout({ content:
             `       
              <div>
              ${req.session.userId}
                 <form method="POST">
                     <input name="email" placeholder="email">
+                    ${getError(errors, 'email')}
                     <input name="password" placeholder="password">
+                    ${getError(errors, 'password')}
                     <input name="passwordConfirmation" placeholder="password confirmation">
+                    ${getError(errors, 'passwordConfirmation')}
                     <button>Sign Up</button>
                 </form>
              </div>
