362ff5ce499dde4d49ee752e824618ba6f5ac277
Vaidotas <skema.vaidotas@gmail.com>
2020-07-13 16:36:20 +0300

    add products routing

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(cookieSession({
     keys: ['asdasdasd']
 }));
 app.use(authRouter);
+app.use(productsRouter);
 
 
 
