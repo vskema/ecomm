3a4313cd0d3ae28771309fff23142d57e0132ade
Vaidotas <skema.vaidotas@gmail.com>
2020-07-16 20:32:07 +0300

    add handle errors middleware

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const {check, validationResult} = require('express-validator');
 
+const { handleErrors } = require('./middlewares');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -19,24 +19,20 @@ router.get('/signup', (req, res) => {
 });
 
 
-router.post('/signup', [requireEmail, requirePassword, requirePasswordConfirmation], async (req, res) => {
-    const errors = validationResult(req);
+router.post('/signup',
+    [requireEmail, requirePassword, requirePasswordConfirmation],
+    handleErrors(signupTemplate),
 
-    if(!errors.isEmpty()) {
-        return res.send(signupTemplate( {req, errors}));
-    }
+    async (req, res) => {
+        const { email, password } = req.body;
 
-    const { email, password, passwordConfirmation } = req.body;
+        // Create a user in our repo to represent this person
+        const user = await usersRepo.create({email: email, password: password});
 
+        // Store the id of that user inside the users cookie
+        req.session.userId = user.id;
 
-    // Create a user in our repo to represent this person
-    const user = await usersRepo.create({email: email, password: password});
-
-    // Store the id of that user inside the users cookie
-    req.session.userId = user.id;
-
-
-    res.send('Account created');
+        res.send('Account created');
 })
 
 router.get('/signout', (req, res) => {
@@ -48,20 +44,14 @@ router.get('/signin', (req, res) => {
     res.send(signinTemplate({}));
 });
 
-router.post('/signin', [requireEmailExist, requireValidPasswordForUser], async (req, res) => {
-    const errors = validationResult(req);
-
-    if(!errors.isEmpty()){
-        return res.send(signinTemplate({errors: errors}));
-    }
-
-    const { email } = req.body;
-
-    const user = await usersRepo.getOneBy({email: email});
-
-    req.session.userId = user.id;
-
-    res.send('You are signed in!!!')
+router.post('/signin',
+    [requireEmailExist, requireValidPasswordForUser],
+    handleErrors(signinTemplate),
+    async (req, res) => {
+        const { email } = req.body;
+        const user = await usersRepo.getOneBy({email: email});
+        req.session.userId = user.id;
+        res.send('You are signed in!!!')
 });
 
 module.exports = router;
--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { validationResult } = require('express-validator');
+const multer = require('multer');
+
+const { handleErrors } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
+const upload = multer({storage: multer.memoryStorage()});
 
 router.get('/admin/products', (req, res) => {
 
@@ -13,10 +16,16 @@ router.get('/admin/products', (req, res) => {
 router.get('/admin/products/new', (req, res) => {
     res.send(productsNewTemplate({}));
 });
-router.post('/admin/products/new', [requireTitle, requirePrice], (req, res) => {
-    const errors = validationResult(req);
-    console.log(errors)
-    res.send('submited');
+router.post('/admin/products/new',
+    upload.single('image'),
+    [requireTitle, requirePrice],
+    handleErrors(productsNewTemplate),
+    async (req, res) => {
+        const image = req.file.toString('base64');
+        const {title, price} = req.body;
+        await productsRepo.create({title, price, image})
+
+        res.send('submited');
 });
 
 module.exports = router;
