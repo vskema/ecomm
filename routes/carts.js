0e03f1284bcfc33b345a02df3b88f34fb578fbc2
Vaidotas <skema.vaidotas@gmail.com>
2020-07-20 20:17:38 +0300

    add cart item delete route

--- routes/carts.js
+++ routes/carts.js
@@ -44,4 +44,16 @@ router.get('/cart', async (req, res) => {
     res.send(cartShowTemplate({ items: cart.items }));
 });
 
+router.post('/cart/products/delete', async (req, res) => {
+
+    const { itemId } = req.body;
+    const cart = await cartsRepo.getOne(req.session.cartId);
+
+    const items = cart.items.filter(item => item.id !== itemId );
+
+    await cartsRepo.update(req.session.cartId, {items: items});
+
+    res.redirect('/cart');
+});
+
 module.exports = router;
