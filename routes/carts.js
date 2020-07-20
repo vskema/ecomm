a495d552d6c7f97b442f3414219453235d24ba32
Vaidotas <skema.vaidotas@gmail.com>
2020-07-20 16:54:22 +0300

    add cart id to cookie

--- routes/carts.js
+++ routes/carts.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const cartsRepo = require('../repositories/carts');
 
 const router = express.Router();
 
-router.post('/cart/products', (req, res) => {
-    console.log(req.body.productId);
+router.post('/cart/products', async (req, res) => {
+    let cart;
+    if(!req.session.cartId){
+        cart = await cartsRepo.create({items: []});
+        req.session.cartId = cart.id;
+    } else{
+         cart = await cartsRepo.getOne(req.session.cartId);
+    }
+    console.log(cart);
+
+
     res.send('product added to cart')
 })
 
