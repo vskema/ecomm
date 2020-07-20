09499c7b660d333609ff6ae94da2a124e9175cdb
Vaidotas <skema.vaidotas@gmail.com>
2020-07-20 18:00:31 +0300

    display cart items

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cartsRepo = require('../repositories/carts');
+const productsRepo = require('../repositories/products');
+const cartShowTemplate = require('../views/carts/show');
 
 const router = express.Router();
 
@@ -11,10 +13,35 @@ router.post('/cart/products', async (req, res) => {
     } else{
          cart = await cartsRepo.getOne(req.session.cartId);
     }
-    console.log(cart);
 
+    const existingItem = cart.items.find(item => item.id === req.body.productId);
+    if(existingItem){
+        existingItem.quantity++;
+    } else {
+        cart.items.push({id: req.body.productId, quantity: 1});
+    }
+
+    await cartsRepo.update(cart.id, {
+        items: cart.items
+    })
 
     res.send('product added to cart')
-})
+});
+
+router.get('/cart', async (req, res) => {
+   if(!req.session.cartId){
+       return res.redirect('/');
+   }
+
+   const cart = await cartsRepo.getOne(req.session.cartId);
+
+   for(let item of cart.items){
+       const product = await productsRepo.getOne(item.id);
+
+       item.product = product;
+   }
+
+    res.send(cartShowTemplate({ items: cart.items }));
+});
 
 module.exports = router;
